c07263d1fad85cd30a7ecc93a24d49b22df31f9e
albim72 <marcin.albiniak72@gmail.com>
2022-07-12 11:49:56 +0200

    Update Module1.vb

--- KLASA123/Module1.vb
+++ KLASA123/Module1.vb
@@ -5,7 +5,27 @@ Module Module1
 
     Sub Main()
 
+        Dim pk As PierwszaKlasa = New PierwszaKlasa(5, 8)
+        pk.printAB()
 
+        Console.WriteLine("*********************************")
+
+        Dim dk As DrugaKlasa = New DrugaKlasa(3, 12, 7)
+        dk.printAB()
+        dk.printABC()
+        Console.WriteLine($"suma a+b+c = {dk.suma()}")
+
+        Console.WriteLine("*********************************")
+
+        Dim tk As TrzeciaKlasa = New TrzeciaKlasa(98, 3, 1, 15)
+        tk.printAB()
+        tk.printABC()
+        tk.printABCD()
+        Console.WriteLine($"suma a+b+c+d = {tk.suma()}")
+
+
+
+        Console.ReadKey()
     End Sub
 
 End Module
