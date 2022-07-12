f24f862ed704a0ea172f4cf4b1f151923fa05ff8
albim72 <marcin.albiniak72@gmail.com>
2022-07-12 11:24:45 +0200

    Create TrzeciaKlasa.vb

---
+++ KLASA123/TrzeciaKlasa.vb
@@ -0,0 +1,9 @@
+'Zbuduj klasę TrzeciaKlasa dziedzczącą klasę DrugaKlasa,
+'dodaj pole Integer -> d oraz przekaż z dziedziczenia (a,b,c)
+'zbuduj metodę printABCD -> wyświetl wszystkie 4 wartości
+'zbuduj metodę suma() -> wykonaj sumowanie wszysktich 4 wartości
+'metoda suma nie jest nową metodą tylko przesłoniętą metodą suma() z DrugaKlasa (Overridable)
+
+Public Class TrzeciaKlasa
+
+End Class
