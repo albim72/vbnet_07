cc9e7295bfdaf45565247e16e9b7797d3c89671f
albim72 <marcin.albiniak72@gmail.com>
2022-07-12 11:37:21 +0200

    Create Module1.vb

---
+++ KLASA123/Module1.vb
@@ -0,0 +1,11 @@
+Module Module1
+
+    'Stwórz po jednym obiekcie dla każdej z 3 klas i wykonaj dla każdego obieektu maksymalną
+    'liczbę dostępnych metod
+
+    Sub Main()
+
+
+    End Sub
+
+End Module
