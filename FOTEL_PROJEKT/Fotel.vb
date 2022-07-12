c369fcaefab0b94a00c494588fb4fad8b637f5ef
albim72 <marcin.albiniak72@gmail.com>
2022-07-12 13:49:57 +0200

    Create Fotel.vb

---
+++ FOTEL_PROJEKT/Fotel.vb
@@ -0,0 +1,14 @@
+'Stwórz klasę Fotel i nadaj przyszłemj obiektowi właściwości:
+'nazwahandlowa -> String
+'matrial -> String
+'szerokosc -> Double
+'wysokosc -> Double
+'glebokosc -> Double
+
+'W module stwórz listę o nazwie fotele przechowującą 6 obiektów klasy Fotel z opisem ich właściwości
+'Wyświetl na konsoli wszystkie fotele wraz z ich właściwościami
+
+
+Public Class Fotel
+
+End Class
