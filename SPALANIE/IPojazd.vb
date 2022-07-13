4ed21da86fe3f027b5e7ac3869c87bb34ca92ccc
albim72 <marcin.albiniak72@gmail.com>
2022-07-13 09:36:46 +0200

    Create IPojazd.vb

---
+++ SPALANIE/IPojazd.vb
@@ -0,0 +1,7 @@
+Public Interface IPojazd
+
+    Function spalanie(spal As Double, odl As Double) As Double
+    Function kosztprzejzadu(spal As Double, odl As Double, cena_l As Double) As Double
+
+
+End Interface
