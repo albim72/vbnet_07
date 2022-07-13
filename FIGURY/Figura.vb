de5078054a158ade0e586f70ffc494871cce3522
albim72 <marcin.albiniak72@gmail.com>
2022-07-13 10:04:08 +0200

    Create Figura.vb

---
+++ FIGURY/Figura.vb
@@ -0,0 +1,19 @@
+Public MustInherit Class Figura
+
+    Public a As Double
+    Public b As Double
+
+    Protected Sub New(a As Double, b As Double)
+        Me.a = a
+        Me.b = b
+    End Sub
+
+    Public Sub Info(msg)
+        Console.WriteLine(msg)
+    End Sub
+
+    Public MustOverride Function policzPole() As Double
+
+
+
+End Class
