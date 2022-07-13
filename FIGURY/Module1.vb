782030e87f40da5b44cf63fde7dd30d92b79e069
albim72 <marcin.albiniak72@gmail.com>
2022-07-13 10:19:44 +0200

    Create Module1.vb

---
+++ FIGURY/Module1.vb
@@ -0,0 +1,18 @@
+Module Module1
+
+    Sub Main()
+
+        Dim pr As Prostokat = New Prostokat(4.5, 7.2)
+        Console.WriteLine($"Pole prostokąta wynosi: {pr.policzPole()}")
+
+        Dim tr As Trojkat = New Trojkat(4.4, 6.7)
+        Console.WriteLine($"Pole trójkąta wynosi: {tr.policzPole()}")
+
+        Dim trp As Trapez = New Trapez(6.8, 4.8, 4.6)
+        Console.WriteLine($"Pole trapezu wynosi: {trp.policzPole()}")
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
