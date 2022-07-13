dd5b748821cc657180d018ec81d0015079666392
albim72 <marcin.albiniak72@gmail.com>
2022-07-13 11:09:05 +0200

    Create Elektryk.vb

---
+++ AUTO/Elektryk.vb
@@ -0,0 +1,10 @@
+Public Class Elektryk : Inherits Auto
+
+    Public Sub New(odleglosc As Double, cena_jedn As Double)
+        MyBase.New(odleglosc, cena_jedn)
+    End Sub
+
+    Public Overrides Function policzKosztPaliwa() As Double
+        Return 14.7 * odleglosc * cena_jedn / 100
+    End Function
+End Class
