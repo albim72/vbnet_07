8864a058b3f1a3c4ad014e5ddfb8e193d86a6068
albim72 <marcin.albiniak72@gmail.com>
2022-07-13 09:54:13 +0200

    Update Pojazd.vb

--- SPALANIE/Pojazd.vb
+++ SPALANIE/Pojazd.vb
@@ -1,4 +1,16 @@
-Public Class Pojazd : Implements IPojazd
+Public Class Pojazd : Implements IPojazd, IDanePojazdu
+
+    Public marka As String
+    Public model As String
+    Public poj As Double
+    Public rocznik As Integer
+
+    Public Sub New(marka As String, model As String, poj As Double, rocznik As Integer)
+        Me.marka = marka
+        Me.model = model
+        Me.poj = poj
+        Me.rocznik = rocznik
+    End Sub
 
     Public Function spalanie(spal As Double, odl As Double) As Double Implements IPojazd.spalanie
         Return spal * 100 / odl
@@ -6,5 +18,9 @@ Public Class Pojazd : Implements IPojazd
 
     Public Function kosztprzejzadu(spal As Double, odl As Double, cena_l As Double) As Double Implements IPojazd.kosztprzejzadu
         Return spalanie(spal, odl) * (odl / 100) * cena_l
+    End Function
+    
+    Public Function pojazdinfo() As String Implements IDanePojazdu.pojazdinfo
+        Return $"Pojazd -> marka: {marka}, model: {model}, pojemność silnika: {poj}, rocznik: {rocznik}."
     End Function
 End Class
