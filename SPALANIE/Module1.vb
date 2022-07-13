6e8361dc05c28c3b7e57cf2415acd909d23e7cca
albim72 <marcin.albiniak72@gmail.com>
2022-07-13 09:54:40 +0200

    Update Module1.vb

--- SPALANIE/Module1.vb
+++ SPALANIE/Module1.vb
@@ -2,7 +2,7 @@ Module Module1
 
     Sub Main()
 
-        Dim pr As IPojazd = New Pojazd()
+        Dim pr As Pojazd = New Pojazd("Jeep", "Cherokee", 4.4, 2019)
         Dim sp As Double
         Dim ko As Double
 
@@ -11,6 +11,7 @@ Module Module1
         Console.WriteLine($"średnie spalanie [l/100km]: {Format(sp, "0.00")}")
 
         Console.WriteLine($"koszt przejazdu: {Format(ko, "0.00")} zł")
+        Console.WriteLine(pr.pojazdinfo())
 
         Console.ReadKey()
 
