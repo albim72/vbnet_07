13e2295d5f11cb8ac437da8aecf23990cbc6354d
albim72 <marcin.albiniak72@gmail.com>
2022-07-14 11:23:59 +0200

    Update form1.vb

--- ELEMENTY_VB/form1.vb
+++ ELEMENTY_VB/form1.vb
@@ -12,4 +12,17 @@ Public Class Form1
         Metoda("to jest druga metoda z opcjonalnym parametrem")
         Metoda("to jest jest trzecia metoda z dwoma parametrami", "Trzecia")
     End Sub
+    
+      Private Function Kwadrat(bok As Integer) As Long
+        Return bok ^ 2
+    End Function
+
+    Private Function Kwadrat(bok As Double) As Double
+        Return bok ^ 2
+    End Function
+    Private Sub btnKw_Click(sender As Object, e As EventArgs) Handles btnKw.Click
+        Dim wynikL As Long = Kwadrat(23)
+        Dim wynikD As Double = Kwadrat(5.89)
+        Metoda($"wynik dla Long: {wynikL}, wynik dla Double: {wynikD}", "Kwadrat")
+    End Sub
 End Class
